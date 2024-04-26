6b7fcd2fc8b7d32a45046f2bcb720cc73fc1e6ed
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-26 12:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+let rad = document.getElementById('radius')
+let vol = document.getElementById('volume')
+
+function volume_sphere(event_details) {
     //Write your code here
-  
+  event_details.preventDefault();
+	let radius = rad.value
+	vol.value = (4/3)*3.14*Math.pow(radius,3)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
