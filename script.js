9b1b0a3717319c361ab35c5a0b8f618f5b7c2ece
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-26 12:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function volume_sphere(event_details) {
     //Write your code here
   event_details.preventDefault();
 	let radius = rad.value
-	vol.value = (4/3)*Math.PI*Math.pow(radius,3)
+	let volume = (4/3)*Math.PI*Math.pow(radius,3)
+	vol.value=volume.toFixed(4)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
